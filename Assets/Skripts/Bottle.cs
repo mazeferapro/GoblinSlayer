9bb082022d2ff02fe9e07ac396a8734694a4677a
Exavi <assassins77718@gmail.com>
2022-03-28 12:42:39 +0300

    +beta esc menu -inv fix

--- Assets/Skripts/Bottle.cs
+++ Assets/Skripts/Bottle.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+
 
 public class Bottle : MonoBehaviour
 {
     public LayerMask Player;
+    public DataBase data;
+    public Item item;
     void OnTriggerEnter2D(Collider2D tr)
     {
         if (tr.gameObject.tag == "Player")
